fb8ccef6a35c1df6cd95083a497d21afe802375e
vmurali100 <vmurali100@gmail.com>
2021-11-19 09:30:29 +0530

    2 files modified, 3 added

--- display.js
+++ display.js
@@ -17,6 +17,7 @@ function displayPersons() {
     var edittd = document.createElement('td');
     var editBtn = document.createElement('button');
     editBtn.setAttribute('class', 'btn btn-warning');
+    editBtn.setAttribute('onclick', 'editPerson(' + i + ')');
     editBtn.innerHTML = 'Edit';
     edittd.appendChild(editBtn);
     myTr.appendChild(edittd);
@@ -25,6 +26,7 @@ function displayPersons() {
     var deletetd = document.createElement('td');
     var deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('class', 'btn btn-danger');
+    deleteBtn.setAttribute('onclick', 'deletePerson(' + i + ')');
     deleteBtn.innerHTML = 'Delete';
     deletetd.appendChild(deleteBtn);
     myTr.appendChild(deletetd);
